3ab54ef11ee5a1ef9ca06d2021d0baa9ef01417b
Christopher Turtle <hosmerangel@gmail.com>
2012-02-18 18:46:19 +0000

    added nested resource for cat events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,27 @@
 class EventsController < ApplicationController
+  respond_to :json, :html
+  
+  before_filter :get_cat
+  
+  def show
+    respond_with(Event.find(params[:id]))
+  end
 
+  def create
+    @event = Event.new(params[:question])
+    if @event.save
+      render :json => @event
+    end
+  end
+  
+  def index
+    @events = @cat.nil? ? Event.all : @cat.events
+    respond_with(@events)
+  end
 
+  private
+  
+  def get_cat
+    @cat = Cat.find(params[:cat_id]) unless params[:cat_id].blank?
+  end
 end
