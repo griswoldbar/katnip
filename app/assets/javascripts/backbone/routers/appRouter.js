4bb230eb8de861124f78c6474508720bf8bb9cbe
Christopher Turtle <hosmerangel@gmail.com>
2012-02-19 17:44:15 +0000

    cat events

--- app/assets/javascripts/backbone/routers/appRouter.js
+++ app/assets/javascripts/backbone/routers/appRouter.js
@@ -19,7 +19,9 @@ Katnip.Routers.AppRouter = Backbone.Router.extend(
 			this.cat = this.catList.get(id);
 			this.cat.getEvents();
 			this.catView = new Katnip.Views.CatView({model:this.cat});
+			this.eventView =new Katnip.Views.EventListView({model: this.cat.events});
 			$('#details').html(this.catView.render().el);
+			$('#details').append(this.eventView.render().el);
 		},
 		
 		catEvents: function(id)
--- app/assets/javascripts/backbone/views/events.js
+++ app/assets/javascripts/backbone/views/events.js
@@ -1,5 +1,29 @@
 Katnip.Views.EventListView = Backbone.View.extend(
 	{
-		
+		tagName: "ul",
+		initialize: function()
+		{
+			this.model.bind("reset",this.render,this);
+		},
+		render: function()
+		{
+			_.each(this.model.models, function(event){
+				$(this.el).append(new Katnip.Views.EventListItemView({model:event}).render().el);
+			},this);
+			return this;
+		}	
+	}
+)
+
+Katnip.Views.EventListItemView = Backbone.View.extend(
+	{
+	 	tagName: "li",
+	 	template: JST['backbone/templates/tpl-cat-events'],
+	 	render: function()
+	 	{
+	 		$(this.el).html(this.template(this.model.toJSON()));
+	 		return this;
+	 	}
+	 
 	}
 )
